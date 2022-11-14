9ff762008e1c2e9ffaa26b1790496acf7c9a24bd
minsu <cocacolazero024@gmail.com>
2022-11-14 23:59:23 +0900

    Create Search Header

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import styles from './search_header.module.css';
 
-const SearchHeader = (props) => <h1>Search Header</h1>;
-
+const SearchHeader = (props) => {
+  return (
+    <header className={styles.header}>
+      <img className={styles.img} src='/images/logo.png' alt='logo' />
+      <h1 className={styles.title}>React Youtube Project</h1>
+      <input className={styles.input} type='search' placeholder='Search' />
+      <button className={styles.button} type='submit'>
+        <img src='/images/search.png' alt='search' />
+      </button>
+    </header>
+  );
+};
 export default SearchHeader;
