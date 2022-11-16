2e5f289599f493512ceb12cedc1c1e22a09d962e
minsu <cocacolazero024@gmail.com>
2022-11-16 23:47:22 +0900

    Style Header CSS

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -4,11 +4,17 @@ import styles from './search_header.module.css';
 const SearchHeader = (props) => {
   return (
     <header className={styles.header}>
-      <img className={styles.img} src='/images/logo.png' alt='logo' />
-      <h1 className={styles.title}>React Youtube Project</h1>
+      <div className={styles.logo_title}>
+        <img className={styles.img} src='/images/logo.png' alt='logo' />
+        <h1 className={styles.title}>React Youtube Project</h1>
+      </div>
       <input className={styles.input} type='search' placeholder='Search' />
       <button className={styles.button} type='submit'>
-        <img src='/images/search.png' alt='search' />
+        <img
+          className={styles.button_image}
+          src='/images/search.png'
+          alt='search'
+        />
       </button>
     </header>
   );
