ed53f616ff11a6b62f0f72f8d17233fd8ecdf656
minsu <cocacolazero024@gmail.com>
2022-11-18 23:58:40 +0900

    Create youtube class

---
+++ src/service/youtube.js
@@ -0,0 +1,16 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+  }
+
+  mostPopular() {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+  }
+}
