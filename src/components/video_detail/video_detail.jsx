a18a451559cffc6c662f9ff779f7d5bef2362cfd
minsu <cocacolazero024@gmail.com>
2022-11-22 01:10:23 +0900

    Add Youtube iframe(player)

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styles from './video_detail.module.css';
 
-const VideoDetail = ({ video }) => {
-  return <h1>{video.snippet.title}</h1>;
-};
+const VideoDetail = ({ video }) => (
+  <section className={styles.detail}>
+    <iframe
+      className={styles.video}
+      type='text/html'
+      width='100%'
+      height='500px'
+      src={`https://www.youtube.com/embed/${video.id}`}
+      frameborder='0'
+      allowfullscreen
+    ></iframe>
+  </section>
+);
 
 export default VideoDetail;
